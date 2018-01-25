f5b48145268f43cda4f4dfdb9cad47a8b03959aa
Allyson Hotchkin <ahotchk1@yahoo.com>
2018-01-25 00:21:51 +0000

    automatically backed up by learn

--- lib/until.rb
+++ lib/until.rb
@@ -1,8 +1,9 @@
 def using_until
         levitation_force = 6
         #your code here
-        levitation_force <=10
-        puts "Wingardium Leviosa"
-        levitation_force +=
+        while levitation_force <=10
+          puts "Wingardium Leviosa"
+          levitation_force +=
+        end
 
 end
